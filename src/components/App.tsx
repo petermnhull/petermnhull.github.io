a114aa6d13ceeea2c1af968efc788781144afb6b
petermnhull <petermnhull@gmail.com>
2022-01-08 19:35:55 +0000

    Removes navigation and routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,8 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home"
 
 const App = () => {
     return (
-        <HashRouter>
-            <Routes>
-                <Route index element={<Home />}/>
-            </Routes>
-        </HashRouter>
+        <Home />
     )
 }
 
