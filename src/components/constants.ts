1b6b71cf613569c8674c25f0f0e859c5c991fe60
petermnhull <petermnhull@gmail.com>
2022-11-24 12:43:26 +0000

    Releases 'Lessons in MLOps' blog post

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,6 +1,10 @@
 export const blogs = [
   {
+    title: "Lessons in MLOps: How To Prioritise Automation",
+    fileName: '2022_11_02_lessons_in_ml_ops'
+  },
+  {
     title: 'Building a Mini Data Warehouse with Athena and Kafka Connect',
     fileName: '2022_10_24_mini_data_warehouse'
-  }
+  },
 ];
